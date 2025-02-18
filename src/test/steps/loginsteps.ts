b2de24adbd845cb7c309aa37bf802e461d0fe940
Punter Code <puntercode@outlook.com>
2025-02-18 16:45:21 +0530

    login feature file

---
+++ src/test/steps/loginsteps.ts
@@ -0,0 +1,6 @@
+import { Given } from "@cucumber/cucumber"
+
+Given('I am on the UPS homepage', async function () {
+    //await page.goto('https://www.ups.com');
+    console.log('Browser opened and navigated to UPS website.');
+  });
