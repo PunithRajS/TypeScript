32d6f5d54f1e26e7f857835d49cdf68865d22dab
Punter Code <puntercode@outlook.com>
2025-02-21 19:36:29 +0530

    updated signup feature file

--- src/test/features/signup.feature
+++ src/test/features/signup.feature
@@ -1,24 +1,24 @@
-Feature: UPS User Sign-Up
+Feature: User Sign-Up
   As a new user
   I want to sign up for a UPS account
-  So that I can access UPS services and manage my shipments
+  So that I can access  services and manage my shipments
 
   Background:
-    Given I am on the UPS sign-up page
+    Given I am on the sign-up page
 
   @positive
   Scenario: Successful sign-up with valid information
     When I enter my first name "John"
     And I enter my last name "Doe"
     And I enter my email address "john.doe@example.com"
-    And I enter my password "SecurePassword123"
+    And I enter my password "Password123"
     And I agree to the terms and conditions
     And I click the "Sign Up" button
     Then I should be directed to the email verification page
     When I enter the verification code sent to my email "123456"
     And I click the "Verify" button
     Then I should see a message confirming successful sign-up
-    And I should be redirected to the UPS home page
+    And I should be redirected to the home page
 
   @negative
   Scenario: Sign-up with already registered email
--- src/test/steps/SignUp.ts
+++ src/test/steps/SignUp.ts
@@ -13,7 +13,7 @@ After(async function () {
   await browser.close();
 });
 
-Given('I am on the UPS sign-up page', async function () {
+Given('I am on the sign-up page', async function () {
     
 });
 
@@ -30,6 +30,10 @@ When('I enter my last name {string}', async function (string) {
 
 When('I enter my email address {string}', async function (string) {
     
+});
+
+When('I enter my password {string}', async function (string) {
+  
 });
 
 When('I agree to the terms and conditions', async function () {
@@ -48,40 +52,13 @@ Then('I should see a message confirming successful sign-up', async function () {
     
 });
 
-
-Then('I should be redirected to the UPS home page', async function () {
+Then('I should be redirected to the home page', async function () {
     
 });
-
-
-Given('I am on the UPS sign-up page', async function () {
-    
-});
-
-
-When('I enter my first name {string}', async function (string) {
-    
-});
-
-
-When('I enter my last name {string}', async function (string) {
-    
-});
-
-
-When('I enter my email address {string}', async function (string) {
-    
-});
-
-When('I agree to the terms and conditions', async function () {
-    
-});
-
 
 Then('I should see an error message indicating the email is already registered', async function () {
     
 });
-
 
 Then('I should remain on the sign-up page', async function () {
     
